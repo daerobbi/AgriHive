810bdd7c5a4c05faba0e540fee7af6ad064f69cd
Dae Robbi <ahmadpratamadaerobbi@gmail.com>
2025-05-23 00:57:32 +0700

    ..

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,7 +27,7 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
