b40664949d32dea4dbfb860f63049710fe70e6fd
Dae Robbi <ahmadpratamadaerobbi@gmail.com>
2025-05-31 23:00:01 +0700

    ..

--- app/Models/kota.php
+++ app/Models/kota.php
@@ -4,13 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
-class kota extends Model
+class Kota extends Model
 {
     use HasFactory;
+
     protected $table = 'kotas';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     protected $fillable = [
-        'nama_kota'];
-}
+        'nama_kota',
+    ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
+}
--- database/migrations/2013_04_20_182614_create_kotas_table.php
+++ database/migrations/2013_04_20_182614_create_kotas_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('kotas', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->timestamps();
             $table->string('nama_kota');
         });
